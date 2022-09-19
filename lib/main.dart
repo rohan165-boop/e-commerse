c5371647269c30a940b60d8819311d70b0b5794f
Rohan <rohanchy165@gmail.com>
2022-09-19 17:39:46 +0545

    sy travel

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:ecommerse/custom_ticket_ui/ticket_homepage.dart';
 import 'package:ecommerse/pages/home.dart';
 import 'package:ecommerse/practice/demo.dart';
 import 'package:ecommerse/route_transition/firt_page.dart';
+import 'package:ecommerse/sy_expedition_travel_challenge/main_page.dart';
+import 'package:ecommerse/sy_expedition_travel_challenge/styles.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -45,15 +47,32 @@ void main() {
 // }
 
 // route transition
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(fontFamily: 'SF Pro Display'),
+//       title: 'Buy Tickets',
+//       home: const FirstPage(),
+//     );
+//   }
+// }
+
+// sy_expedition_travel
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'SF Pro Display'),
-      title: 'Buy Tickets',
-      home: const FirstPage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: mainBlack,
+      ),
+      home: const MainPage(),
     );
   }
 }
