396c5d79d2fd48718620fedfe96d8472b9a2431f
Rohan <rohanchy165@gmail.com>
2022-10-13 17:19:12 +0545

    ticketclipper

--- lib/features/home_page/view/home_screen.dart
+++ lib/features/home_page/view/home_screen.dart
@@ -1,35 +1,47 @@
-import 'package:ecommerse/widgets/custom_app_bar_widgets.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
+import 'package:ecommerse/custom_practice/sy_expedition_travel_challenge/styles.dart';
+import 'package:ecommerse/widgets/custom_app_bar_widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: const CustomAppBar(
         title: "Zero To Unicorn",
       ),
-      body: ClipPath(
-        clipper: MyClipper(),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          height: MediaQuery.of(context).size.height / 4,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Color(0xFF3383CD),
-                Color(0xFF11249F),
+      body: Center(
+        child: ClipPath(
+          clipper: MyClipper(),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            height: MediaQuery.of(context).size.height / 4,
+            width: 200,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color(0xFF3383CD),
+                  Color(0xFF11249F),
+                ],
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 2,
+                  width: 200,
+                  color: Colors.red,
+                )
               ],
             ),
           ),
         ),
-
       ),
       // Column(
       //   children: [
@@ -52,19 +64,86 @@ class HomeScreen extends StatelessWidget {
 }
 
 class MyClipper extends CustomClipper<Path> {
+  
+
+  // final double right;
+  // final double holeRadius;
+  // MyClipper({
+  //   required this.right,
+  //   required this.holeRadius,
+  // });
   @override
   Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height);
-    path.quadraticBezierTo(
-        size.width / 3, size.height / 3, size.width , size.height / 5);
-    path.lineTo(size.width, 0);
-    path.close();
+
+        Path path = Path();
+
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0.0);
+
+    path.addOval(Rect.fromCircle(
+        center: Offset(0.0, size.height / 2), radius: 20.0));
+    path.addOval(Rect.fromCircle(
+        center: Offset(size.width, size.height / 2), radius: 20.0));
+
     return path;
+
+    //  final path = Path()
+    //   ..moveTo(0, 0)
+    //   ..lineTo(size.width - right - holeRadius, 0.0)
+    //   ..arcToPoint(
+    //     Offset(size.width - right, 0),
+    //     clockwise: false,
+    //     radius: const Radius.circular(5),
+    //   )
+    //   ..lineTo(size.width, 0.0)
+    //   ..lineTo(size.width, size.height)
+    //   ..lineTo(size.width - right, size.height)
+    //   ..arcToPoint(
+    //     Offset(size.width - right - holeRadius, size.height),
+    //     clockwise: false,
+    //     radius: const Radius.circular(5),
+    //   );
+
+    //   path.lineTo(0.0, size.height);
+
+    // path.close();
+    // return path;
+
+    // var path = Path();
+    // path.lineTo(0, size.height-100);
+    // path.quadraticBezierTo(size.width/10, size.height/10, size.width/7, size.height-100);
+    // path.lineTo(size.width, size.height-100);
+    // path.lineTo(size.width, 0);
+    // path.close();
+    // return path;
+
+
+    // Path path = Path();
+    // path.lineTo(0, size.height);
+    // var curXPos = 0.0;
+    // var curYPos = size.height;
+    // var increment = size.width / 1;
+    // while (curXPos < size.width) {
+    //   curXPos += increment;
+    //   path.arcToPoint(Offset(curXPos, curYPos), radius: const Radius.circular(2));
+    // }
+    //  path.lineTo(size.width, 0);
+    //  return path;
+
+    // arck model
+
+    // var path = Path();
+    // path.lineTo(0, size.height);
+    // path.quadraticBezierTo(
+    //     size.width / 3, size.height / 3, size.width , size.height / 5);
+    // path.lineTo(size.width, 0);
+    // path.close();
+    // return path;
   }
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
+    return true;
   }
 }
